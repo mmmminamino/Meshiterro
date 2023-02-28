0fc3a7642d3c49db93b376988e82b112047a904b
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-02-28 07:03:02 +0000

    [16章途中]meshiterro

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   def get_profile_image
     unless profile_image.attached?
       file_path=Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io:File.open(file_path),filename:'default_omage.jpg',content_type:'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename:'default_image.jpg', content_type:'image/jpeg')
     end
-    profile_image.variant(resize_to_limit:[width,height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
